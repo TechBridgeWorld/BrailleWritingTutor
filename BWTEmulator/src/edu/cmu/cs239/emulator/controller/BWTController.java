e41515f5862d6332a6ef15031d683f5040840439
Lucas Ray <lucas@lucasray.com>
2013-02-03 13:21:29 -0500

    simple ui

--- BWTEmulator/src/edu/cmu/cs239/emulator/controller/BWTController.java
+++ BWTEmulator/src/edu/cmu/cs239/emulator/controller/BWTController.java
@@ -12,78 +12,78 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/BWTController")
 public class BWTController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+  private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public BWTController() {
-    	
+
         super();
     }
 
-	/**
-	 * 
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		printRequestInfo(request);
-		sendJSON(response);
-	}
+  /**
+   *
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	/**
-	 * Handle POST request.
-	 * TODO: Currently this is handled exactly the same as GET request. Need implementation
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Handling POST request as GET");
-		doGet(request,response);
-	}
-	
-	/**
-	 * Print out the incoming http request
-	 * @param request
-	 */
-	private void printRequestInfo(HttpServletRequest request){
-		if(request == null){
-			System.out.println("Request is null.");
-			return;
-		}
-		String url = request.getRequestURL().toString();
-		String query = request.getQueryString();
-		String port = Integer.toString(request.getLocalPort());
-		java.util.Enumeration<String> e = request.getParameterNames();
-		System.out.println("----Printing Request-----");
-		System.out.println("url: " +url);
-		System.out.println("query: " + query);
-//		if(query!=null){
-//			for(String s= "";e.hasMoreElements();s=e.nextElement()){
-//				System.out.println(s);
-//			}
-//		}
-		System.out.println("Port: " + port);
-		
-	}
-	
-	/**
-	 * 
-	 * @param request
-	 * @param response
-	 */
-	private void sendJSON(HttpServletResponse response){
-		if(response == null) return;
-		String text = "{ message: Java servlet says hello world}";
-	    response.setContentType("text/plain");  
-	    response.setCharacterEncoding("UTF-8"); 
-	    try {
-			response.getWriter().write(text);
-		} catch (IOException e) {
-			System.out.println("Exception in response.write");
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}     
-	}
-	
+    printRequestInfo(request);
+    sendJSON(response);
+  }
+
+  /**
+   * Handle POST request.
+   * TODO: Currently this is handled exactly the same as GET request. Need implementation
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    System.out.println("Handling POST request as GET");
+    doGet(request,response);
+  }
+
+  /**
+   * Print out the incoming http request
+   * @param request
+   */
+  private void printRequestInfo(HttpServletRequest request){
+    if(request == null){
+      System.out.println("Request is null.");
+      return;
+    }
+    String url = request.getRequestURL().toString();
+    String query = request.getQueryString();
+    String port = Integer.toString(request.getLocalPort());
+    java.util.Enumeration<String> e = request.getParameterNames();
+    System.out.println("----Printing Request-----");
+    System.out.println("url: " +url);
+    System.out.println("query: " + query);
+//    if(query!=null){
+//      for(String s= "";e.hasMoreElements();s=e.nextElement()){
+//        System.out.println(s);
+//      }
+//    }
+    System.out.println("Port: " + port);
+
+  }
+
+  /**
+   *
+   * @param request
+   * @param response
+   */
+  private void sendJSON(HttpServletResponse response){
+    if(response == null) return;
+    String text = "{ message: Java servlet says hello world}";
+      response.setContentType("text/plain");
+      response.setCharacterEncoding("UTF-8");
+      try {
+      response.getWriter().write(text);
+    } catch (IOException e) {
+      System.out.println("Exception in response.write");
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
 }
--- source/app/scripts/controllers/ButtonPress.js
+++ source/app/scripts/controllers/ButtonPress.js
@@ -1,9 +1,10 @@
-/** @file Request.js
- *  @brief Class that represents an AJAX request to our local server. To be used
- *         to communicate between the JAVA servlet server and the front end.
+/** @file ButtonPress.js
+ *  @brief Class that represents a button press and sends an AJAX request to
+ *         our local server. To be used to communicate between the JAVA
+ *         servlet server and the front end.
  *
  *  Usage: new Request({
- *           'url': 'http://www.example.com/',
+ *           'button': 's5-2-3',
  *           'data': JSON.stringify({'answer': 42}),
  *           'type': 'GET',
  *           'success': function(data) { console.log(data); },
@@ -17,7 +18,7 @@ $(document).ready(function() {
   /** @brief Constructor for a request.
    *
    *  @param options Options for the request. These include:
-   *           -url: URL of the request (e.g. "/login" or "/sendData")
+   *           -button: Button ID for the request (e.g. "s5-2-3")
    *           -data: JSON object representing data we want sent. If request is
    *                  a POST, this will be in the body. If a GET, it will be in
    *                  the URL.
@@ -26,8 +27,8 @@ $(document).ready(function() {
   *            -failure: Callback to be used on failure.
    *           -TODO: more?
    */
-  var Request = function(options) {
-    this.url = options.url;
+  window.ButtonPress = function(options) {
+    this.button = options.button;
     this.data = options.data;
     this.type = options.type;
     this.success = options.success;
@@ -38,9 +39,14 @@ $(document).ready(function() {
 
   /** @brief Makes the request.
    */
-  var __make_request = function() {
-//    $.ajax({
-//    });
-  // TODO
+  window.ButtonPress.prototype.__make_request = function() {
+    // __TEMP__ is bad
+    var __TEMP__ = "foobar";
+    $.ajax({
+      url: '/' + __TEMP__ + '/sendBytes.do?button=' + this.button,
+      type: this.type,
+      success: this.success,
+      error: this.failure
+    });
   };
 });
