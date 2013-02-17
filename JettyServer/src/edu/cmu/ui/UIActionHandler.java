70dfdc504226754456a04f9155479b47c84525ea
Zi Wang <wangzi0112@gmail.com>
2013-02-17 14:29:49 -0500

    new server

---
+++ JettyServer/src/edu/cmu/ui/UIActionHandler.java
@@ -0,0 +1,31 @@
+package edu.cmu.ui;
+
+import edu.cmu.server.JettyServer;
+
+public class UIActionHandler {
+
+	private JettyServer js;
+	
+	public UIActionHandler(){
+		js = new JettyServer();
+	}
+	
+	public void startServer(){
+		js.startServer();
+		System.out.println("Starting server..");
+	}
+	
+	public void stopServer(){
+		js.stopServer();
+	}
+	
+	public void restartServer(){
+		js.restartServer();
+	}
+	
+	
+	public String getServerStatus(){
+		return "Running";
+	}
+	
+}
