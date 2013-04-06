89a24047641a5da4a8dd95ca5d565a75828e7fbb
Lucas Ray <lucas@lucasray.com>
2013-04-06 19:52:52 -0400

    awesome lookin' record css

--- source/JettyServer/src/html/scripts/controllers/recording.js
+++ source/JettyServer/src/html/scripts/controllers/recording.js
@@ -1,5 +1,5 @@
 /**
-* Handles the recording feature of the emulator. 
+* Handles the recording feature of the emulator.
 * Allows the user to record a sequence of button presses and resend
 * the exact sequence later.
 */
@@ -16,30 +16,31 @@ var __SAVED_RECORDING_QUEUE =[];
 * and the send recording button
 */
 function __registerButtons(){
-	var toggleButton = $("#recording_toggle");
-	var sendButton = $("#send_recording");
+  var toggleButton = $("#recording_toggle");
+  var sendButton = $("#send_recording");
 
-	sendButton.html(__SENT_RECORDING_TEXT);
-	toggleButton.html(__START_RECORDING_TEXT);
+  sendButton.html(__SENT_RECORDING_TEXT);
+  toggleButton.html(__START_RECORDING_TEXT);
 
-	toggleButton.on('click',function(e){
-		window.recording = !window.recording;
-		if(window.recording){
-			toggleButton.html(__STOP_RECORDING_TEXT);
-			sendButton.attr('disabled', 'disabled');
-		}
-		else{
-			toggleButton.html(__START_RECORDING_TEXT);
-			sendButton.removeAttr('disabled');
-			__saveRecording();
-			__updateRecordingDropdown();
-		}
+  toggleButton.on('click',function(e){
+    window.recording = !window.recording;
+    $("#recording_toggle").toggleClass("active");
+    if(window.recording){
+      toggleButton.html(__STOP_RECORDING_TEXT);
+      sendButton.attr('disabled', 'disabled');
+    }
+    else{
+      toggleButton.html(__START_RECORDING_TEXT);
+      sendButton.removeAttr('disabled');
+      __saveRecording();
+      __updateRecordingDropdown();
+    }
 
-	});
+  });
 
-	sendButton.on('click',function(e){
-		sendRecording();
-	});
+  sendButton.on('click',function(e){
+    sendRecording();
+  });
 }
 
 /**
@@ -49,8 +50,8 @@ function initializeRecording(){
     window.recording = false;
     window.recordingQueue = [];
     window.prevTimeStamp = undefined;
-	__registerButtons();
-	__updateRecordingDropdown();
+  __registerButtons();
+  __updateRecordingDropdown();
 
 }
 
@@ -58,127 +59,127 @@ function initializeRecording(){
 * Append the button press and its time stamp to the recording queue.
 */
 function appendToRecordingQueue(button, eventType){
-	if(button == undefined ||eventType == undefined){
-		window.LOG_WARNING("Invalid key press. Can't add to recording queue.");
-		return;
-	}
-	var delta;
-	var curr = new Date();
-	if(prevTimeStamp == undefined){
-		delta = 0;
-	}
-	else{
-		delta = curr.getTime() - prevTimeStamp;
-	}
-	prevTimeStamp = curr.getTime();
+  if(button == undefined ||eventType == undefined){
+    window.LOG_WARNING("Invalid key press. Can't add to recording queue.");
+    return;
+  }
+  var delta;
+  var curr = new Date();
+  if(prevTimeStamp == undefined){
+    delta = 0;
+  }
+  else{
+    delta = curr.getTime() - prevTimeStamp;
+  }
+  prevTimeStamp = curr.getTime();
 
-	recordingQueue.push({
-		button : button,
-		timeStamp : delta,
-		eventType : eventType
-	});
+  recordingQueue.push({
+    button : button,
+    timeStamp : delta,
+    eventType : eventType
+  });
 }
 
 /**
 * Send the recordings and clear the recording queue.
 */
 function sendRecording(){
-	if(recording){
-		LOG_ERROR("Can't send saved recordings while recording new one.");
-		return;
-	}
-	var recordingIndex = $("#recording_select").val();
-	if(recordingIndex >= __SAVED_RECORDING_QUEUE.length){
-		LOG_ERROR("No such recording");
-		return;
-	}
-	var queue = __SAVED_RECORDING_QUEUE[recordingIndex].queue;
+  if(recording){
+    LOG_ERROR("Can't send saved recordings while recording new one.");
+    return;
+  }
+  var recordingIndex = $("#recording_select").val();
+  if(recordingIndex >= __SAVED_RECORDING_QUEUE.length){
+    LOG_ERROR("No such recording");
+    return;
+  }
+  var queue = __SAVED_RECORDING_QUEUE[recordingIndex].queue;
 
-	if(!__isValidRecordingQueue(queue)){
-		LOG_ERROR("Invalid recording: " + __SAVED_RECORDING_QUEUE[recordingIndex].name);
-		return;
-	}
-	__sendRecordingHelper(queue,0);
-	
+  if(!__isValidRecordingQueue(queue)){
+    LOG_ERROR("Invalid recording: " + __SAVED_RECORDING_QUEUE[recordingIndex].name);
+    return;
+  }
+  __sendRecordingHelper(queue,0);
+
 }
 
 function __sendRecordingHelper(queue, index){
 
-	if(index >= queue.length){
-		LOG_ERROR("Invalid recording queue index.");
-		return;
-	}
-	var eventType = queue[index].eventType;
-	var button = queue[index].button;
-	if(eventType===__KEY_DOWN){
-		button.press_down();
-	}
-	else if(eventType === __KEY_UP){
-		button.press_up();
-	}
-	else{
-		LOG_ERROR("Invalid event type in recording queue. " + eventType);
-	}
+  if(index >= queue.length){
+    LOG_ERROR("Invalid recording queue index.");
+    return;
+  }
+  var eventType = queue[index].eventType;
+  var button = queue[index].button;
+  if(eventType===__KEY_DOWN){
+    button.press_down();
+  }
+  else if(eventType === __KEY_UP){
+    button.press_up();
+  }
+  else{
+    LOG_ERROR("Invalid event type in recording queue. " + eventType);
+  }
 
-	//check the next button in the queue
-	if(index+1 >= queue.length){
-		LOG_INFO("Done sending recording queue. Last index is " + index);
-		return;//done with the queue.
-	}
-	var delta = queue[index+1].timeStamp;
+  //check the next button in the queue
+  if(index+1 >= queue.length){
+    LOG_INFO("Done sending recording queue. Last index is " + index);
+    return;//done with the queue.
+  }
+  var delta = queue[index+1].timeStamp;
 
-	//after delta, send the next button
-	setTimeout(function(){
-		__sendRecordingHelper(queue, index+1);
-	}, delta);
+  //after delta, send the next button
+  setTimeout(function(){
+    __sendRecordingHelper(queue, index+1);
+  }, delta);
 }
 
 function __saveRecording(){
-	var name = $("#recording_name").val();
-	if(name === undefined || name.trim().length==0){
-		name = "Recording " + (__SAVED_RECORDING_QUEUE.length+1);
-	}
-	__SAVED_RECORDING_QUEUE.push({
-		queue : recordingQueue,
-		name  : name.trim()
-	});
-	prevTimeStamp = undefined;
-	recordingQueue = [];
-	$("#recording_name").val("");
+  var name = $("#recording_name").val();
+  if(name === undefined || name.trim().length==0){
+    name = "Recording " + (__SAVED_RECORDING_QUEUE.length+1);
+  }
+  __SAVED_RECORDING_QUEUE.push({
+    queue : recordingQueue,
+    name  : name.trim()
+  });
+  prevTimeStamp = undefined;
+  recordingQueue = [];
+  $("#recording_name").val("");
 }
 
 /**
-* 
+*
 */
 function __updateRecordingDropdown(){
-	var dropdown = $("#recording_select");
-	var options = "";
-	for(var i=0;i<__SAVED_RECORDING_QUEUE.length;i++){
-		options += "<option value='" + i +"'>" + __SAVED_RECORDING_QUEUE[i].name +"</option>";
-	}
-	dropdown.html(options);
+  var dropdown = $("#recording_select");
+  var options = "";
+  for(var i=0;i<__SAVED_RECORDING_QUEUE.length;i++){
+    options += "<option value='" + i +"'>" + __SAVED_RECORDING_QUEUE[i].name +"</option>";
+  }
+  dropdown.html(options);
 }
 
 /**
 * Check if the given recording queue is valid.
 */
 function __isValidRecordingQueue(queue){
-	if(queue == undefined || 
-		(! queue instanceof Array) ||
-		queue.length == 0){
-		return false;
-	} 
-	for(var i=0;i<queue.length;i++){
-		var curr = queue[i];
-		if(curr.button == undefined ||
-			curr.eventType == undefined ||
-			[__KEY_UP,__KEY_DOWN].indexOf(curr.eventType)==-1 ||
-			curr.timeStamp == undefined ||
-			curr.timeStamp < 0){
-			return false;
-		}
-	}
-	return true;
+  if(queue == undefined ||
+    (! queue instanceof Array) ||
+    queue.length == 0){
+    return false;
+  }
+  for(var i=0;i<queue.length;i++){
+    var curr = queue[i];
+    if(curr.button == undefined ||
+      curr.eventType == undefined ||
+      [__KEY_UP,__KEY_DOWN].indexOf(curr.eventType)==-1 ||
+      curr.timeStamp == undefined ||
+      curr.timeStamp < 0){
+      return false;
+    }
+  }
+  return true;
 
 }
 
